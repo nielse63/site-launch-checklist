a7299467bc16a48118549b069e48add60286ec16
Erik Nielsen <erik@312development.com>
2016-10-23 23:03:18 -0500

    Updated test files

--- test/index.js
+++ test/index.js
@@ -1,10 +1,14 @@
 
 const path = require( 'path' );
+require('dotenv').config({
+	path: path.resolve(__dirname, '..', '.env')
+});
 const docroot = path.resolve(__dirname, 'sample');
 const checklist = require('../index')({
 	docroot : docroot
 });
 
-checklist.getSiteInfo().then(data => {
-	console.log(data);
-});
+// checklist.getSiteInfo().then(data => {
+// 	console.log(data);
+// });
+checklist.run();
