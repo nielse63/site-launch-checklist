a5753a37e0732d62d2b9febe3e0dc7c9270d13bf
Erik Nielsen <erik@312development.com>
2016-10-25 16:56:03 -0500

    Committing new directory structure

---
+++ tasks/lib.js
@@ -0,0 +1,34 @@
+
+'use strict';
+
+import gulp from 'gulp'
+import babel from "gulp-babel"
+import runSequence from 'run-sequence'
+import del from 'del'
+require('shelljs/global');
+
+const inDir = 'src/lib/**/*.js';
+const outDir = 'lib';
+
+gulp.task('lib:clean', () => del(outDir, {
+	dot : true
+}));
+
+gulp.task("lib:babel", function () {
+	return gulp.src(inDir)
+		.pipe(babel())
+		.pipe(gulp.dest(outDir));
+});
+
+gulp.task("lib:lint", function () {
+	exec('./node_modules/.bin/eslint ' + inDir + ' --fix');
+});
+
+gulp.task("lib", function (done) {
+	runSequence(
+		'lib:clean',
+		'lib:lint',
+		'lib:babel',
+		done
+	);
+});
