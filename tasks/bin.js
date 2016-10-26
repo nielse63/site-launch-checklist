c25ce441d51e9755941de100301074970d34d3d3
Erik Nielsen <erik@312development.com>
2016-10-26 04:07:13 -0500

    Committing tasks before babeling

--- tasks/bin.js
+++ tasks/bin.js
@@ -6,39 +6,34 @@ import babel from "gulp-babel"
 import eslint from "gulp-eslint"
 import changed from "gulp-changed"
 import runSequence from 'run-sequence'
-import jshint from 'gulp-jshint'
-import stylish from 'jshint-stylish'
 import del from 'del'
 require('shelljs/global');
 
-const inDir = 'src/lib/**/*.js';
-const outDir = 'lib';
+const inDir = 'src/bin/**/*.js';
+const outDir = 'bin';
 
-gulp.task('lib:clean', () => {
-	del(outDir, {
-		dot : true
-	})
+var checkPerms = (function() {
+	return function() {
+		console.log(this);
+	};
+}())
+
+gulp.task('bin:clean', () => {
+	del(outDir + '/*.js')
 });
 
-gulp.task("lib:babel", function () {
+gulp.task("bin:babel", function () {
 	return gulp.src(inDir)
 		.pipe(changed(outDir))
 		.pipe(babel())
 		.pipe(gulp.dest(outDir));
 });
 
-gulp.task('lib:jshint', function() {
-	return gulp.src(inDir)
-		.pipe(changed(inDir))
-		.pipe(jshint())
-		.pipe(jshint.reporter(stylish));
-});
-
-gulp.task("lib:lint", function () {
+gulp.task("bin:lint", function () {
 	exec('./node_modules/.bin/eslint ' + inDir + ' --fix');
 });
 
-gulp.task("lib", function (done) {
+gulp.task("bin", function (done) {
 	runSequence(
 		'lib:clean',
 		'lib:lint',
@@ -48,6 +43,6 @@ gulp.task("lib", function (done) {
 });
 
 // Watch files for changes & reload
-gulp.task('watch:lib', () => {
-	gulp.watch([inDir], ['lib:babel']);
+gulp.task('watch:bin', () => {
+	gulp.watch([inDir], ['bin:babel']);
 });
