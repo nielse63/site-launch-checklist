192732dec1e81248d5ce5c60980684390c11e46f
Erik Nielsen <erik@312development.com>
2016-11-18 00:59:02 -0600

    Created new reporters

---
+++ src/lib/reporters/index.js
@@ -0,0 +1,10 @@
+
+const table = require('./table')
+const json = require('./json')
+const html = require('./html')
+
+module.exports = {
+	table,
+	json,
+	html,
+}
--- src/lib/reporters/json.js
+++ src/lib/reporters/json.js
@@ -1,42 +1,36 @@
 
 const utils = require('../utils')
 const _ = require('lodash')
-const Table = require('cli-table');
-const colors = require('../constants').colors
 
 module.exports = function(collections) {
 	const keys = Object.keys(collections)
+	let output = {}
 	let rules = keys.map((key) => {
 		return collections[key]
 	})
 	rules = _.flatten(rules)
-
 	let grouped = utils.groupBy(rules, (rule) => {
 		return rule.get('docs').category;
 	});
 	grouped = _.flatten(grouped)
 
-	// set up table
-	const table = new Table({
-		style: {
-			head: ['green'],
-			border: ['white'],
-		},
-		head : ['Category', 'Name', 'Passed', 'Message']
-	})
-
 	grouped.forEach((rule) => {
 		const category = rule.get('docs').category
 		const name = rule.get('name')
 		const passed = ! rule.get('failed')
-		const color = passed ? 'green' : 'red'
-		const symbol = colors[color]( passed ? '✓' : '✗' )
 		const messaging = rule.get('messaging')
 		const message = passed ? messaging.success : messaging.fail
 
-		table.push([category, name, symbol, message])
+		if( ! output.hasOwnProperty(category) ) {
+			output[category] = []
+		}
+		output[category].push({
+			name,
+			passed : String(passed),
+			message,
+		})
 	})
 
 	// print table
-	return '\n' + table.toString()
+	return JSON.stringify(output)
 }
