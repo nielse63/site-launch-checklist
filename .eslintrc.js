210b84b0e65e94010e2c34804c00ece45b2f6f23
Erik Nielsen <erik@312development.com>
2016-11-23 03:55:56 -0600

    Updated eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+        "mocha": true
+    },
+    "extends": "standard",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+};
