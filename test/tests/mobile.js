37c8c375a540a12605c4402700c2f709e1d0ebb2
Erik Nielsen <erik@312development.com>
2016-12-12 15:29:42 -0600

    Update mobile test

--- test/tests/mobile.js
+++ test/tests/mobile.js
@@ -1,6 +1,35 @@
-// import assert from 'assert';
-// import launchChecklist from '../../lib';
+import assert from 'assert'
+import _ from 'lodash'
+import cheerio from 'cheerio'
+import https from 'https'
 
-// describe('tests/mobile', function () {
-//   // ...
-// })
+import request from '../../lib/request'
+import mobile from '../../lib/tests/mobile'
+
+let output
+const TEST_URL = 'https://cliquestudios.com/'
+
+describe('tests/mobile', function () {
+  this.timeout(10000)
+
+  before(function(done) {
+    mobile(TEST_URL).then(results => {
+      output = results
+      done()
+    }, err => {
+      throw new Error(err)
+    })
+  })
+
+  it('Mobile should return object', function() {
+    assert(
+      _.isPlainObject(output)
+    )
+  })
+
+  it('Mobile should have desired keys', function() {
+    assert(_.has(output, 'passed'))
+    assert(_.has(output, 'info'))
+    assert(_.has(output, 'reason'))
+  })
+})
