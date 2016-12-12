2e8bb5e4388571965c90f71c93652f9670554fa4
Erik Nielsen <erik@312development.com>
2016-12-12 15:26:16 -0600

    Expand test coverage for broken links

--- test/tests/broken-links.js
+++ test/tests/broken-links.js
@@ -1,6 +1,41 @@
-// import assert from 'assert';
-// import launchChecklist from '../../lib';
+import assert from 'assert'
+import _ from 'lodash'
+import cheerio from 'cheerio'
+import https from 'https'
 
-// describe('tests/broken-links', function () {
-//   // ...
-// })
+import request from '../../lib/request'
+import brokenLinks from '../../lib/tests/broken-links'
+
+let output
+const TEST_URL = 'https://cliquestudios.com/'
+
+describe('tests/broken-links', function () {
+  this.timeout(10000)
+
+  before(function(done) {
+    request(TEST_URL, (err, data) => {
+      if(err) {
+        throw new Error(err)
+      }
+
+      brokenLinks(data.body, data.url).then(results => {
+        output = results
+        done()
+      }, err => {
+        throw new Error(err)
+      })
+    })
+  })
+
+  it('Broken Links should return object', function() {
+    assert(
+      _.isPlainObject(output)
+    )
+  })
+
+  it('Broken Links should have desired keys', function() {
+    assert(_.has(output, 'passed'))
+    assert(_.has(output, 'info'))
+    assert(_.has(output, 'reason'))
+  })
+})
--- test/tests/seo.js
+++ test/tests/seo.js
@@ -1,6 +1,38 @@
-// import assert from 'assert';
-// import launchChecklist from '../../lib';
+import assert from 'assert'
+import _ from 'lodash'
+import cheerio from 'cheerio'
+import https from 'https'
 
-// describe('tests/seo', function () {
-//   // ...
-// })
+import request from '../../lib/request'
+import seo from '../../lib/tests/seo'
+
+let $
+let output
+const TEST_URL = 'https://www.facebook.com/'
+
+describe('tests/seo', function () {
+
+  before(function(done) {
+    request(TEST_URL, (err, data) => {
+      if(err) {
+        throw new Error(err)
+      }
+
+      $ = cheerio.load(data.body)
+      output = seo($)
+      done()
+    })
+  })
+
+  it('SEO should return object', function() {
+    assert(
+      _.isPlainObject(output)
+      )
+  })
+
+  it('SEO should have desired keys', function() {
+    assert(_.has(output, 'passed'))
+    assert(_.has(output, 'info'))
+    assert(_.has(output, 'reason'))
+  })
+})
