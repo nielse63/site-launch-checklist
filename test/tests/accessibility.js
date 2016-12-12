f9838411c1193f29aa3945bad48dde9fa83caab0
Erik Nielsen <erik@312development.com>
2016-12-12 16:20:06 -0600

    Expand test coverage

--- test/tests/accessibility.js
+++ test/tests/accessibility.js
@@ -1,6 +1,48 @@
-// import assert from 'assert';
-// import launchChecklist from '../../lib';
+import assert from 'assert'
+import _ from 'lodash'
+import https from 'https'
 
-// describe('tests/accessibility', function () {
-//   // ...
-// })
+import request from '../../lib/request'
+import accessibility from '../../lib/tests/accessibility'
+
+let output
+const TEST_URL = 'https://cliquestudios.com/'
+
+describe('tests/accessibility', function () {
+  this.timeout(20000)
+
+  before(function(done) {
+    accessibility(TEST_URL).then(results => {
+      output = results
+      done()
+    }, err => {
+      throw new Error(err)
+    })
+  })
+
+  it('Accessibility should return object', function() {
+    assert(
+      _.isPlainObject(output)
+    )
+  })
+
+  it('Accessibility should have desired keys', function() {
+    assert(_.has(output, 'passed'))
+    assert(_.has(output, 'info'))
+    assert(_.has(output, 'reason'))
+  })
+
+  it('Accessibility should catch invalid urls', function(done) {
+    accessibility('not_a_url')
+      .catch(err => {
+        assert.throws(
+          function() {
+            throw new Error(err)
+          },
+          /Error opening url/
+        )
+        done()
+      }
+    )
+  })
+})
--- test/tests/css-validation.js
+++ test/tests/css-validation.js
@@ -1,6 +1,59 @@
-// import assert from 'assert';
-// import launchChecklist from '../../lib';
+import assert from 'assert'
+import _ from 'lodash'
+import https from 'https'
 
-// describe('tests/css-validator', function () {
-//   // ...
-// })
+import cssValidation from '../../lib/tests/css-validation'
+
+let output
+const TEST_URL = 'https://www.facebook.com/'
+
+describe('tests/css-validation', function () {
+  this.timeout(20000)
+
+  before(function(done) {
+    cssValidation(TEST_URL, 200).then(data => {
+      output = data
+      done()
+    })
+  })
+
+  it('CSS Validation should return object', function() {
+    assert(
+      _.isPlainObject(output)
+      )
+  })
+
+  it('CSS Validation should have desired keys', function() {
+    assert(_.has(output, 'passed'))
+    assert(_.has(output, 'info'))
+    assert(_.has(output, 'reason'))
+  })
+
+  it('CSS Validation should catch 400 status', function(done) {
+    cssValidation(TEST_URL, 404)
+      .catch(err => {
+        assert.throws(
+          function() {
+            throw new Error(err)
+          },
+          /Cannot validate CSS\./
+        )
+        done()
+      }
+    )
+  })
+
+  it('CSS Validation should fail on invalid urls', function(done) {
+    cssValidation('not_a_url', 200)
+      .catch(err => {
+        assert.throws(
+          function() {
+            throw new Error(err)
+          },
+          /Invalid server response/
+        )
+        done()
+      }
+    )
+  })
+})
