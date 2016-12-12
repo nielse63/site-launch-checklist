937669681ad39db27f25f51ee3b6d2030804e735
Erik Nielsen <erik@312development.com>
2016-12-12 15:07:01 -0600

    Update security test coverage

--- test/tests/security.js
+++ test/tests/security.js
@@ -1,6 +1,38 @@
-// import assert from 'assert';
-// import launchChecklist from '../../lib';
+import assert from 'assert'
+import _ from 'lodash'
+import cheerio from 'cheerio'
+import https from 'https'
 
-// describe('tests/security', function () {
-//   // ...
-// })
+import request from '../../lib/request'
+import security from '../../lib/tests/security'
+
+let headers
+let output
+const TEST_URL = 'https://cliquestudios.com/'
+
+describe('tests/security', function () {
+
+  before(function(done) {
+    request(TEST_URL, (err, data) => {
+      if(err) {
+        throw new Error(err)
+      }
+
+      headers = data.headers
+      output = security(headers)
+      done()
+    })
+  })
+
+  it('Security should return object', function() {
+    assert(
+      _.isPlainObject(output)
+    )
+  })
+
+  it('Security should have desired keys', function() {
+    assert(_.has(output, 'passed'))
+    assert(_.has(output, 'info'))
+    assert(_.has(output, 'reason'))
+  })
+})
