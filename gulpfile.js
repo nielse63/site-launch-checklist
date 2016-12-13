13a8ec76f0304c29fdf5dbe5c6eab458748b7fc6
Erik Nielsen <erik@312development.com>
2016-12-12 23:03:02 -0600

    Updated test coverage submission

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const lec = require('gulp-line-ending-corrector')
 const babel = require('gulp-babel')
 const del = require('del')
 const isparta = require('isparta')
+const shelljs = require('shelljs')
 
 // Initialize the babel transpiler so ES2015 files gets compiled
 // when they're loaded
@@ -36,43 +37,64 @@ gulp.task('test', ['pre-test'], cb => {
     './test/**/*.js',
     '!**/.*',
   ])
-    .pipe(plumber())
-    .pipe(mocha())
-    .on('error', err => {
-      mochaErr = err
-    })
-    .pipe(istanbul.writeReports())
-    .on('end', () => {
-      cb(mochaErr)
-    })
+  .pipe(plumber())
+  .pipe(mocha())
+  .on('error', err => {
+    mochaErr = err
+  })
+  .pipe(istanbul.writeReports())
+  .on('end', () => {
+    cb(mochaErr)
+  })
 })
 
 gulp.task('line-ending-corrector', () => gulp.src('lib/cli.js')
-    .pipe(excludeGitignore())
-    .pipe(lec())
-    .pipe(gulp.dest('.')))
+  .pipe(excludeGitignore())
+  .pipe(lec())
+  .pipe(gulp.dest('.')))
 
 gulp.task('cli', () => gulp.src('lib/cli.js')
-    .pipe(excludeGitignore())
-    .pipe(babel())
-    .pipe(gulp.dest('./dist')))
+  .pipe(excludeGitignore())
+  .pipe(babel())
+  .pipe(gulp.dest('./dist')))
 
 gulp.task('watch', () => {
   gulp.watch(['lib/**/*.js', 'test/**'], ['babel'])
   gulp.watch(['lib/cli.js'], ['cli'])
 })
 
-gulp.task('coveralls', ['test'], () =>
-  // if (!process.env.CI) {
-  //   return
-  // }
+gulp.task('coveralls', ['test'], () => {
+  if (!process.env.CI) {
+    return
+  }
 
-   gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-    .pipe(coveralls()))
+  gulp.src(path.join(__dirname, 'coverage/lcov.info'))
+    .pipe(coveralls())
+})
 
 gulp.task('babel', ['clean'], () => gulp.src('lib/**/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('dist')))
+  .pipe(babel())
+  .pipe(gulp.dest('dist')))
+
+gulp.task('submit-coverage', () => {
+  if (process.env.CLI) {
+    return
+  }
+
+  shelljs.exec([
+    'CODECLIMATE_REPO_TOKEN=aa305c42ae223fab11cc52ac1d57536e438c09de9cbd03d3a6c8dbdeec1ffc23',
+    './node_modules/.bin/codeclimate-test-reporter',
+    '< ./coverage/lcov.info',
+  ].join(' '), {
+    async: true,
+    silent: true,
+  }, (code, stdout, stderr) => {
+    if (code) {
+      throw new Error(stderr)
+    }
+    console.log(stdout)
+  })
+})
 
 gulp.task('clean', () => del('dist'))
 
