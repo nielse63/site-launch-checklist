254d922492ea36cdd6a1b6780a22d9efc589f3e0
Erik Nielsen <erik@312development.com>
2016-12-12 19:20:05 -0600

    Update test coverage

--- test/tests/html-validation.js
+++ test/tests/html-validation.js
@@ -1,6 +1,56 @@
-// import assert from 'assert';
-// import launchChecklist from '../../lib';
+import assert from 'assert'
+import _ from 'lodash'
+import request from '../../lib/request'
+import htmlValidation from '../../lib/tests/html-validation'
 
-// describe('tests/html-validator', function () {
-//   // ...
-// })
+let output
+const TEST_URL = 'http://www.facebook.com/'
+
+describe('tests/html-validation', function () {
+  this.timeout(20000)
+
+  before(done => {
+    request(TEST_URL, (err, data) => {
+      if(err) {
+        throw new Error(err)
+      }
+
+      htmlValidation(data.body, data.url).then(results => {
+        output = results
+        done()
+      })
+    })
+  })
+
+  it('HTML Validation should be a function', () => {
+    assert(
+      _.isFunction(htmlValidation),
+    )
+  })
+
+  it('HTML Validation should return object', () => {
+    assert(
+      _.isPlainObject(output),
+    )
+  })
+
+  it('HTML Validation should have desired keys', () => {
+    assert(_.has(output, 'passed'))
+    assert(_.has(output, 'info'))
+    assert(_.has(output, 'reason'))
+  })
+
+  it('HTML Validation should fail on invalid urls', done => {
+    htmlValidation('', 'not_a_url')
+      .catch(err => {
+        assert.throws(
+          () => {
+            throw new Error(err)
+          },
+          Error,
+        )
+        done()
+      },
+    )
+  })
+})
--- test/tests/index.js
+++ test/tests/index.js
@@ -1,6 +1,32 @@
-// import assert from 'assert';
-// import launchChecklist from '../../lib';
+import assert from 'assert'
+import _ from 'lodash'
+import request from '../../lib/request'
+import tests from '../../lib/tests'
 
-// describe('tests', function () {
-//   // ...
-// })
+let output
+const TEST_URL = 'http://www.facebook.com/'
+
+describe('tests', function () {
+  before(done => {
+    request(TEST_URL, (err, data) => {
+      if(err) {
+        throw new Error(err)
+      }
+
+      output = tests(data)
+      done()
+    })
+  })
+
+  it('Tests should be a function', () => {
+    assert(
+      _.isFunction(tests),
+    )
+  })
+
+  it('Tests should return an array', () => {
+    assert(
+      _.isArray( output )
+    )
+  })
+})
