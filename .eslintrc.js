bb17993b89a1008dbad0dfb5aa0860f9084f67be
Erik Nielsen <erik@312development.com>
2016-12-12 21:42:22 -0600

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true,
-        "mocha": true
-    },
-    "extends": "airbnb",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-      "semi": ["error", "never"],
-      "no-param-reassign": "warn",
-      "arrow-parens": ["error", "as-needed"],
-      "consistent-return": ["error", { "treatUndefinedAsUnspecified": true }],
-      "max-statements": ["error", 10]
-    }
+  "root": true,
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "extends": "airbnb",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "semi": ["error", "never"],
+    "no-param-reassign": "warn",
+    "arrow-parens": ["error", "as-needed"],
+    "consistent-return": "warn",
+    "max-statements": ["error", 10],
+  }
 };
---
+++ test/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "env": {
+    "mocha": true
+  },
+  "rules": {
+    "prefer-arrow-callback": "off",
+    "max-statements": ["error", 30],
+    "func-names": "off"
+  }
+};
